77c6643fecda66e18323360d137843370b810686
Tahiya Chowdhury <tahiya.chowdhury@rutgers.edu>
2023-11-16 07:24:19 +0000

    added mediapipe function

--- app.py
+++ app.py
@@ -26,6 +26,13 @@ import csv
 import subprocess
 import pathlib
 
+# import libraries for video processing
+import cv2
+import mediapipe as mp
+import csv
+import tempfile
+
+
 #a few more libraries
 import torch
 import whisper
@@ -35,6 +42,8 @@ import librosa
 
 import opensmile
 import base64
+from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
+import moviepy.editor as mp
 
 
 # TO DO: 
@@ -75,20 +84,25 @@ def run():
     #message after uploading the file
     # source: https://github.com/tyler-simons/backgroundremoval/blob/main/bg_remove.py
     def convert_vid2audio(video_upload, audio_file):
-        command = "ffmpeg -i {$video_upload} -acodec pcm_s16le -ar 16000 -ac 2 {$audio_file}"
-        subprocess.call(command, shell=True)
-
+        audio_file = subprocess.run(["ffmpeg", "-i", video_upload.name, "-acodec", "pcm_s16le", "-ar", "16000", "-ac", "2", "pipe:"], stdout=subprocess.PIPE).stdout
+        #subprocess.run('ffmpeg -i {$video_upload.name} {$audio_file}',shell=True)
+        
+        #subprocess.call(command, shell=True)
+        #clip = mp.VideoFileClip(video_upload)
+        #audio_file = clip.audio.write_audiofile("converted_" + video_upload.name.split('.')[0]+".wav", codec = 'wav')
+        
         st.download_button("Download your audio", audio_file, "converted_"+video_upload.name.split('.')[0]+".wav", "audio/wav")        
 
-    with st.sidebar:
-        st.subheader("Data Preparation")
+    #with st.sidebar:
+        #st.subheader("Data Preparation")
 
-        st.markdown("Convert video (mp4) to audio (wav)")
-        video_upload = st.file_uploader("Upload your video file here", type=["mp4"], accept_multiple_files=False,label_visibility="visible", key= 'video_audio')
+        #st.markdown("Convert video (mp4) to audio (wav)")
+        #video_upload = st.file_uploader("Upload your video file here", type=["mp4"], accept_multiple_files=False,label_visibility="visible", key= 'video_audio')
         
-        if video_upload:
-            audio_file = "converted_" + video_upload.name.split('.')[0]+".wav"
-            convert_vid2audio(video_upload, audio_file)
+        #if video_upload:
+            #audio_file = tempfile.NamedTemporaryFile(delete=False)
+            #audio_file = "converted_" + video_upload.name.split('.')[0]+".wav"
+            #convert_vid2audio(video_upload, audio_file)
 
     #st.divider()
     
@@ -172,8 +186,89 @@ def run():
 
     st.divider()
 
+    st.subheader("I want movement from a video file")
+
+    def get_movement(movement_upload):
+        # Initialize MediaPipe Pose and Drawing utilities
+        mp_pose = mp.solutions.pose
+        mp_drawing = mp.solutions.drawing_utils
+        pose = mp_pose.Pose()
+
+        tfile = tempfile.NamedTemporaryFile(delete=False)
+        tfile.write(movement_upload.read())
+
+        # Open the video file
+        cap = cv2.VideoCapture(tfile.name)
+
+        frame_number = 0
+        csv_List = []
+
+        while cap.isOpened():
+            ret, frame = cap.read()
+            if not ret:
+                break
+
+            # Convert the frame to RGB
+            frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+            # Process the frame with MediaPipe Pose
+            result = pose.process(frame_rgb)
+
+            # Draw the pose landmarks on the frame
+            if result.pose_landmarks:
+                #print(result.pose_landmarks.landmark)
+                #mp_drawing.draw_landmarks(frame, result.pose_landmarks, mp_pose.POSE_CONNECTIONS)
+
+                # Add the landmark coordinates to the list and print them
+                #write_landmarks_to_csv(result.pose_landmarks.landmark, frame_number, csv_data)
+                for idx, landmark in enumerate(result.pose_landmarks.landmark):
+                    #print(f"{mp_pose.PoseLandmark(idx).name}: (x: {landmark.x}, y: {landmark.y}, z: {landmark.z})")
+                    csv_List.append([frame_number, mp_pose.PoseLandmark(idx).name, landmark.x, landmark.y, landmark.z])
+                
+                #poseMarkList = []
+                #for idx, landmark in enumerate(result.pose_landmarks.landmark):
+                    #print(f"{mp_pose.PoseLandmark(idx).name}: (x: {landmark.x}, y: {landmark.y}, z: {landmark.z})")
+                    #poseMarkList.append([mp_pose.PoseLandmark(idx).name.landmark.x, landmark.y, landmark.z])
+
+                # stratify the list
+                #flat_list = [item for sublist in poseMarkList for item in sublist]
+                #flat_list.insert([0, frame_number])
+                #csv_List.append(flat_list)
+
+            # Display the frame
+            #cv2.imshow('MediaPipe Pose', frame)
+
+            # Exit if 'q' key is pressed
+            #if cv2.waitKey(1) & 0xFF == ord('q'):
+                #break
+
+            frame_number += 1
+
+        #cap.release()
+        #cv2.destroyAllWindows()
+
+        # Save the CSV data to a file
+        #with open(output_csv, 'a+', newline='') as csvfile:
+            #csv_writer = csv.writer(csvfile)
+            #csv_writer.writerow(['frame_number', 'landmark', 'x', 'y', 'z'])
+            #csv_writer.writerows(csv_List)
+
+        df = pd.DataFrame(csv_List, columns = ['frame_number', 'landmark', 'x', 'y', 'z']) 
+        final_df = df.to_csv(index=False)
+        st.download_button("Download your movement features", final_df, 'movement_features_' + movement_upload.name.split('.')[0] + '.csv', "txt/csv")
+
+    movement_upload = st.file_uploader("Upload your video file here", type=["mp4"], accept_multiple_files=False,label_visibility="visible", key= 'movement')
+    
+    if movement_upload:
+        #output_csv = 'movement_'+movement_upload.name.split('.')[0]+'.csv'
+        get_movement(movement_upload)
+
+    st.divider()
+
     st.subheader("I have feedback/ideas for this toolbox") 
     st.link_button("Sure, let's go to the feedback form", "https://forms.gle/SPojkeqLhXsKVJK1A", help=None, type="secondary", disabled=False, use_container_width=False)
+
+
 
 
 # misc: sharing the app with others: https://docs.github.com/en/codespaces/developing-in-a-codespace/forwarding-ports-in-your-codespace
